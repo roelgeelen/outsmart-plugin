a52011fc5732eb486bf1089dc56e8850b2a7aa8a
R. Geelen <roel-geelen@hotmail.com>
2022-11-14 10:46:50 +0100

    add employees

---
+++ src/main/java/com/differentdoors/outsmart/models/Employees.java
@@ -0,0 +1,28 @@
+package com.differentdoors.outsmart.models;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Employees {
+    private static Map<String, String> employees = new HashMap<>() {
+        {
+            put("1", "Thomas");
+            put("6", "Max");
+            put("9", "Pascal");
+            put("10", "Jay");
+            put("11", "Bart");
+            put("15", "Mathijs");
+            put("71850", "Steve");
+            put("71853", "Patrick");
+            put("71851", "Remco");
+            put("73710", "Lars");
+            put("74368", "Teun");
+            put("89356", "Luuk");
+            put("83298", "Jelle");
+            put("89358", "Ruud");
+        }
+    };
+
+    public static String getEmployee(String id) {
+        return employees.get(id);
+    }
+}
